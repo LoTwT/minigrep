b9ddf31a208716cc73ade8d1f65e4d1136f0881f
LoTwT <709937065@qq.com>
2023-07-08 10:26:10 +0800

    refactor: split to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    // let config = parse_config(&args);
 
     // 对 build 返回的 `Result` 进行处理
     let config = Config::build(&args).unwrap_or_else(|err| {
@@ -17,41 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = &args[1].clone();
-//     let file_path = &args[2].clone();
-
-//     Config (query, file_path)
-// }
